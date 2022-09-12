beb63d65025f9555a2636f03c0d283fdaf3dfc1b
Safaid-Ansari <safaidansari07@gmail.com>
2022-09-12 12:19:01 +0530

    Updates

---
+++ precache-manifest.f12969bb18d0c0c907b12363072afc1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d9546f0e9778a5b7c72c64ae38c56a",
+    "url": "/Ipod-Mini-Project/index.html"
+  },
+  {
+    "revision": "b156223fbe11290acfd2",
+    "url": "/Ipod-Mini-Project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "36fd1636dba86add5ee7",
+    "url": "/Ipod-Mini-Project/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b156223fbe11290acfd2",
+    "url": "/Ipod-Mini-Project/static/js/2.747f51bc.chunk.js"
+  },
+  {
+    "revision": "3afc3f7b055b07aac8edf61359703488",
+    "url": "/Ipod-Mini-Project/static/js/2.747f51bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fd1636dba86add5ee7",
+    "url": "/Ipod-Mini-Project/static/js/main.cc9745f4.chunk.js"
+  },
+  {
+    "revision": "97c7f8697df3ae0e2ea0",
+    "url": "/Ipod-Mini-Project/static/js/runtime-main.a4cfcf8e.js"
+  },
+  {
+    "revision": "9ba16188fa31aa053935b27151e30f78",
+    "url": "/Ipod-Mini-Project/static/media/Senorita.9ba16188.mp3"
+  },
+  {
+    "revision": "72d353e6a7a4095309644001e65f14d2",
+    "url": "/Ipod-Mini-Project/static/media/safaid.72d353e6.JPG"
+  }
+]);
